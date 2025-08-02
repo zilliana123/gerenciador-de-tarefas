dcde624e724bd87c6003c41e8edb2a558a29f33f
Zilliana Sousa <33105906+zilliana123@users.noreply.github.com>
2025-08-01 21:01:46 -0300

    fix: remoção do checkbox

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 import * as React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
 
-export default function CheckboxList() {
-  const [checked, setChecked] = React.useState([0]);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
+function TaskList() {
   return (
     <div
       style={{
@@ -32,47 +14,23 @@ export default function CheckboxList() {
         alignItems: "center",
       }}
     >
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 600,
-          bgcolor: "background.paper",
-        }}
-        alignItems="center"
-      >
-        {[0, 1, 2, 3].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
-
-          return (
-            <ListItem
-              key={value}
-              secondaryAction={
-                <IconButton edge="end" aria-label="Edit">
-                  <EditIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton
-                role={undefined}
-                onClick={handleToggle(value)}
-                dense
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.includes(value)}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ "aria-labelledby": labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={`Tarefa ${value + 1}`} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {[1, 2, 3, 4].map((value) => (
+          <ListItem
+            key={value}
+            disableGutters
+            secondaryAction={
+              <IconButton aria-label="Edit">
+                <EditIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary={`Tarefa ${value}`} />
+          </ListItem>
+        ))}
       </List>
     </div>
   );
 }
+
+export default TaskList;
